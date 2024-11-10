953394da1766c097b3a58b64abc3d551c36a7340
quagthanh <dinhquangthanh11@gmail.com>
2024-11-10 12:00:39 +0700

    HFW-67 fix:[Backend] CRUD Tech and Category

--- api/router/categoryRouter.go
+++ api/router/categoryRouter.go
@@ -9,7 +9,7 @@ import (
 func setUpCategoryRouter(router *mux.Router, handler *handlers.Handler) {
 	router.HandleFunc("/category", handlers.GetCategory).Methods("GET")
 	router.HandleFunc("/category/{id}", handlers.GetCategoryByID).Methods("GET")
-	router.HandleFunc("/category", handlers.CreateCategory).Methods("POST")
+	router.HandleFunc("/category/create", handlers.CreateCategory).Methods("POST")
 	router.HandleFunc("/category/{id}", handlers.DeleteCategoryByID).Methods("DELETE")
-	router.HandleFunc("/category/update/{id}", handlers.UpdateCategoryByID).Methods("POST")
+	router.HandleFunc("/category/{id}/update", handlers.UpdateCategoryByID).Methods("POST")
 }
--- api/router/techRouter.go
+++ api/router/techRouter.go
@@ -9,7 +9,7 @@ import (
 func setUpTechRouter(router *mux.Router, handler *handlers.Handler) {
 	router.HandleFunc("/tech", handlers.GetTech).Methods("GET")
 	router.HandleFunc("/tech/{id}", handlers.GetTechByID).Methods("GET")
-	router.HandleFunc("/tech", handlers.CreateTech).Methods("POST")
+	router.HandleFunc("/tech/create", handlers.CreateTech).Methods("POST")
 	router.HandleFunc("/tech/{id}", handlers.DeleteTechByID).Methods("DELETE")
-	router.HandleFunc("/tech/update/{id}", handlers.UpdateTechByID).Methods("POST")
+	router.HandleFunc("/tech/{id}/update", handlers.UpdateTechByID).Methods("POST")
 }
